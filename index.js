f2979501dedb5d5752e89cd545f5f9da16009c8d
Jamling <li.jamling@gmail.com>
2016-07-19 15:19:48 +0800

    add console plugin

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ helper.register('gh_repos', gh.repos);
 helper.register('gh_releases', gh.releases);
 helper.register('gh_contents', gh.contents);
 helper.register('gh_edit', gh.raw);
-helper.register('gh_cache_dir', gh.cacheDir);//internal helper
+helper.register('gh_cache_dir', gh.cacheDir);// internal helper
 helper.register('gh_read_cache', gh.read);
 helper.register('gh_write_cache', gh.write);
 
 hexo.extend.generator.register('github', require('./lib/generator'));
+
+// console
+var c = require('./lib/console.js');
+hexo.extend.console.register('github', c.desc, c.opts, c.github);
